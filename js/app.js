88284c6a01acedc2925b66b754b090830e2a491e
fxzqf <fxzqf@163.com>
2022-08-31 22:15:12 +0800

    1

--- js/app.js
+++ js/app.js
@@ -15,13 +15,13 @@ class app {
             yield this.jssdk.ready();
         });
         this.jssdk = WebOfficeSDK.config({
-            url: "https://www.kdocs.cn/l/cagNbUYJX08f?R=%2FS%2F4",
+            url: "https://www.kdocs.cn/l/cagNbUYJX08f",
             mount: document.getElementById("custom-mount"),
         });
         console.log(this.jssdk.iframeReady);
-        this.start();
         if (!this.jssdk.iframeReady)
-            this.jssdk.iframe.src = "https://account.wps.cn/?qrcode=kdocs&logo=kdocs&accessid=AK20210823OPGONG&from=v1-web-kdocs-login&cb=https%3A%2F%2Faccount.wps.cn%2Fapi%2Fv3%2Fsession%2Fcorrelate%2Fredirect%3Ft%3D1661241340991%26appid%3D375024576%26cb%3Dhttps%253A%252F%252Fwww.kdocs.cn%252FsingleSign4CST%253Fcb%253Dhttps://www.kdocs.cn/l/coO0iEfp4s1c";
+            this.jssdk.iframe.src = "https://account.wps.cn/?logo=kdocs&accessid=AK20210823OPGONG&from=v1-web-kdocs-share&cb=https%3A%2F%2Faccount.wps.cn%2Fapi%2Fv3%2Fsession%2Fcorrelate%2Fredirect%3Ft%3D1661953655502%26appid%3D375024576%26cb%3Dhttps%253A%252F%252Fwww.kdocs.cn%252FsingleSign4CST%253Fcb%253Dhttps%25253A%25252F%25252Fwww.kdocs.cn%25252Fcheck%25252Fphone%25252Fbind%25253Fcb%25253Dhttps://www.kdocs.cn/l/cagNbUYJX08f&qrcode=kdocs&signupbind=false";
+        this.start();
     }
 }
 window.onload = () => {
