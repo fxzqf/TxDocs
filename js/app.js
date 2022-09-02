d53198516dd8e5d1d3d8857773a0fb6882e00ed1
fxzqf <45503962+fxzqf@users.noreply.github.com>
2022-09-02 15:34:14 +0800

    Create app.js

--- js/app.js
+++ js/app.js
@@ -13,14 +13,27 @@ class app {
     constructor() {
         this.start = () => __awaiter(this, void 0, void 0, function* () {
             yield this.jssdk.ready();
+            const app1 = this.jssdk.Application;
+            // 接收全局广播
+            app1.Sub.OnBroadcast = (e) => __awaiter(this, void 0, void 0, function* () {
+                console.log('接收全局广播', e);
+            });
+            // 公共处理对象
+            const Public = yield app1.Public;
+            // 发送全局广播
+            const result = yield Public.SendBroadcast({
+                Data: { message: '测试' }
+            });
+            //const range = await this.jssdk.Application.Range('A1')
+            // 设置公式：A1 = A2 + A3
+            //range.Formula = '=A2+A3'
         });
         this.jssdk = WebOfficeSDK.config({
             url: "https://www.kdocs.cn/l/cagNbUYJX08f?R=%2FS%2F4",
             mount: document.getElementsByClassName("custom-mount")[0],
         });
-        if (!this.jssdk.iframeReady)
-            this.jssdk.iframe.src = "https://account.wps.cn/?qrcode=kdocs&logo=kdocs&accessid=AK20210823OPGONG&from=v1-web-kdocs-login&cb=https%3A%2F%2Faccount.wps.cn%2Fapi%2Fv3%2Fsession%2Fcorrelate%2Fredirect%3Ft%3D1661241340991%26appid%3D375024576%26cb%3Dhttps%253A%252F%252Fwww.kdocs.cn%252FsingleSign4CST%253Fcb%253Dhttps://www.kdocs.cn/l/coO0iEfp4s1c";
         this.start();
+        //if (!this.jssdk.iframeReady) this.jssdk.iframe.src = "https://account.wps.cn/?qrcode=kdocs&logo=kdocs&accessid=AK20210823OPGONG&from=v1-web-kdocs-login&cb=https%3A%2F%2Faccount.wps.cn%2Fapi%2Fv3%2Fsession%2Fcorrelate%2Fredirect%3Ft%3D1661241340991%26appid%3D375024576%26cb%3Dhttps%253A%252F%252Fwww.kdocs.cn%252FsingleSign4CST%253Fcb%253Dhttps://www.kdocs.cn/l/coO0iEfp4s1c";
     }
 }
 window.onload = () => {
