2b9899addd08e304e777d2e5a8d4f0a4758f66f1
fxzqf <fxzqf@163.com>
2022-09-03 08:35:20 +0800

    1

--- js/app.js
+++ js/app.js
@@ -11,21 +11,21 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 class app {
     constructor() {
+        this.jssdk = WebOfficeSDK.config({
+            url: "https://www.kdocs.cn/l/cagNbUYJX08f?R=%2FS%2F4",
+            mount: document.getElementsByClassName("custom-mount")[0],
+        });
         //get Application(): any {
         //  return (async () => {return await this.jssdk.Application;})()
         //}
         this.start = () => __awaiter(this, void 0, void 0, function* () {
             yield this.jssdk.ready();
-            this.Application = yield this.jssdk.Application;
             //const range = await this.jssdk.Application.Range('A1')
             // 设置公式：A1   = A2 + A3
             //range.Formula = '=A2+A3'
         });
-        this.jssdk = WebOfficeSDK.config({
-            url: "https://www.kdocs.cn/l/cagNbUYJX08f?R=%2FS%2F4",
-            mount: document.getElementsByClassName("custom-mount")[0],
-        });
         this.start();
+        this.Application = (() => __awaiter(this, void 0, void 0, function* () { return (yield this.jssdk.Application); }))();
         //if (!this.jssdk.iframeReady) this.jssdk.iframe.src = "https://account.wps.cn/?qrcode=kdocs&logo=kdocs&accessid=AK20210823OPGONG&from=v1-web-kdocs-login&cb=https%3A%2F%2Faccount.wps.cn%2Fapi%2Fv3%2Fsession%2Fcorrelate%2Fredirect%3Ft%3D1661241340991%26appid%3D375024576%26cb%3Dhttps%253A%252F%252Fwww.kdocs.cn%252FsingleSign4CST%253Fcb%253Dhttps://www.kdocs.cn/l/coO0iEfp4s1c";
     }
 }
--- ts/app.ts
+++ ts/app.ts
@@ -1,15 +1,14 @@
 ///<reference path="../js/weboffice.d.ts"/>
 "use strict";
 class app {
-  jssdk: IWps;
+  private jssdk=WebOfficeSDK.config({
+    url: "https://www.kdocs.cn/l/cagNbUYJX08f?R=%2FS%2F4",
+    mount: document.getElementsByClassName("custom-mount")[0] as HTMLElement,
+  });
   Application:any;    
   constructor() {
-    this.jssdk = WebOfficeSDK.config({
-      url: "https://www.kdocs.cn/l/cagNbUYJX08f?R=%2FS%2F4",
-      mount: document.getElementsByClassName("custom-mount")[0] as HTMLElement,
-    });
-
     this.start();
+    this.Application =(async () => {return (await this.jssdk.Application)})();
 
     //if (!this.jssdk.iframeReady) this.jssdk.iframe.src = "https://account.wps.cn/?qrcode=kdocs&logo=kdocs&accessid=AK20210823OPGONG&from=v1-web-kdocs-login&cb=https%3A%2F%2Faccount.wps.cn%2Fapi%2Fv3%2Fsession%2Fcorrelate%2Fredirect%3Ft%3D1661241340991%26appid%3D375024576%26cb%3Dhttps%253A%252F%252Fwww.kdocs.cn%252FsingleSign4CST%253Fcb%253Dhttps://www.kdocs.cn/l/coO0iEfp4s1c";
   }
@@ -21,7 +20,7 @@ class app {
 
   start = async () => {
     await this.jssdk.ready();
-    this.Application = await this.jssdk.Application;
+    
      
     //const range = await this.jssdk.Application.Range('A1')
 
