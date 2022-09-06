10a0659bcfaa0485078d493195a16471e14c7f7d
fxzqf <fxzqf@163.com>
2022-09-06 08:26:44 +0800

    1

--- js/app.js
+++ js/app.js
@@ -9,37 +9,35 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-class app {
+class App {
     constructor() {
-        this.jssdk = WebOfficeSDK.config({
-            url: "https://www.kdocs.cn/l/cagNbUYJX08f?R=%2FS%2F4",
-            mount: document.getElementsByClassName("custom-mount")[0],
-            onHyperLinkOpen: (obj) => __awaiter(this, void 0, void 0, function* () {
-                console.log(obj.linkUrl);
-                const app1 = this.jssdk.Application;
-                console.log(app1);
-                const Public = yield app.Public;
-                // 发送全局广播
-                const result = yield Public.SendBroadcast({
-                    Data: { message: '测试' }
-                });
-            }),
-            onToast: ({ msg, action }) => { alert(msg); },
-        });
-        //get Application(): any {
-        //  return (async () => {return await this.jssdk.Application;})()
-        //}
-        this.start = () => __awaiter(this, void 0, void 0, function* () {
+        this.Start = () => __awaiter(this, void 0, void 0, function* () {
             yield this.jssdk.ready();
-            //const range = await this.jssdk.Application.Range('A1')
-            // 设置公式：A1   = A2 + A3
-            //range.Formula = '=A2+A3'
+            this.application = this.jssdk.Application;
+            const operatorsInfo = yield this.application.ActiveWorkbook.GetOperatorsInfo();
+            console.log(operatorsInfo);
         });
-        this.start();
-        (() => __awaiter(this, void 0, void 0, function* () { this.Application = yield this.jssdk.Application; }))();
-        //if (!this.jssdk.iframeReady) this.jssdk.iframe.src = "https://account.wps.cn/?qrcode=kdocs&logo=kdocs&accessid=AK20210823OPGONG&from=v1-web-kdocs-login&cb=https%3A%2F%2Faccount.wps.cn%2Fapi%2Fv3%2Fsession%2Fcorrelate%2Fredirect%3Ft%3D1661241340991%26appid%3D375024576%26cb%3Dhttps%253A%252F%252Fwww.kdocs.cn%252FsingleSign4CST%253Fcb%253Dhttps://www.kdocs.cn/l/coO0iEfp4s1c";
+        this.Config = {
+            url: "https://kdocs.cn/l/cgPO0CnUJPTR",
+            mount: document.getElementsByClassName("custom-mount")[0],
+            onToast(toastData) { alert(toastData.action); },
+            commonOptions: {
+                isShowTopArea: false,
+                isShowHeader: true,
+                isIframeViewFullscreen: false,
+                isParentFullscreen: false,
+                isBrowserViewFullscreen: false
+            },
+            otlOptions: { loadOptions: "https://www.baidu.com" }
+        };
+        this.jssdk = WebOfficeSDK.config(this.Config);
+        this.Start();
+        // alert(this.Application);
+    }
+    get Application() {
+        return this.application;
     }
 }
 window.onload = () => {
-    new app();
+    new App();
 };
--- ts/app.ts
+++ ts/app.ts
@@ -1,47 +1,69 @@
 ///<reference path="../js/weboffice.d.ts"/>
 "use strict";
-class app {
-  private jssdk = WebOfficeSDK.config({
+class App {
+  private Config: IConfig;
+  private application: any;
+  jssdk: IWps;
+  get Application() {
+
+    return this.application;
+  }
+
+  constructor() {
+    this.Config = {
+      url: "https://kdocs.cn/l/cgPO0CnUJPTR",
+      mount: document.getElementsByClassName("custom-mount")[0] as HTMLElement,
+      onToast(toastData) { alert(toastData.action); },
+      commonOptions: {
+        isShowTopArea: false, // 隐藏顶部区域（头部和工具栏）
+        isShowHeader: true, // 隐藏头部区域
+        isIframeViewFullscreen: false,
+        isParentFullscreen: false,
+        isBrowserViewFullscreen: false
+      },
+      otlOptions: { loadOptions: "https://www.baidu.com" }
+    };
+    this.jssdk = WebOfficeSDK.config(this.Config);
+    this.Start();
+    // alert(this.Application);
+  }
+
+  Start = async () => {
+    await this.jssdk.ready();
+    this.application = this.jssdk.Application;
+    const operatorsInfo = await this.application.ActiveWorkbook.GetOperatorsInfo();
+    console.log(operatorsInfo);
+
+  }
+
+
+
+  /*
+  this.jssdk = WebOfficeSDK.config({
     url: "https://www.kdocs.cn/l/cagNbUYJX08f?R=%2FS%2F4",
+    
     mount: document.getElementsByClassName("custom-mount")[0] as HTMLElement,
     onHyperLinkOpen: async (obj: { linkUrl: string }) => {
       console.log(obj.linkUrl);
       const app1 = this.jssdk.Application;
-      console.log(app1);
-      const Public = await app.Public;
-      // 发送全局广播
-      const result = await Public.SendBroadcast({
-        Data: { message: '测试' }
-      });
     },
-    onToast: ({ msg, action }) => { alert(msg) },
+    onToast: ({ msg, action }) => { alert(action) },
   });
-  Application: any;
-  constructor() {
-    this.start();
-    (async () => { this.Application = await this.jssdk.Application; })();
+  (async () => { await this.jssdk.ready(); })();
+  this.Application = this.jssdk.Application;
+  alert(this.Application);
+*/
 
-    //if (!this.jssdk.iframeReady) this.jssdk.iframe.src = "https://account.wps.cn/?qrcode=kdocs&logo=kdocs&accessid=AK20210823OPGONG&from=v1-web-kdocs-login&cb=https%3A%2F%2Faccount.wps.cn%2Fapi%2Fv3%2Fsession%2Fcorrelate%2Fredirect%3Ft%3D1661241340991%26appid%3D375024576%26cb%3Dhttps%253A%252F%252Fwww.kdocs.cn%252FsingleSign4CST%253Fcb%253Dhttps://www.kdocs.cn/l/coO0iEfp4s1c";
-  }
+
+
+  //if (!this.jssdk.iframeReady) this.jssdk.iframe.src = "https://account.wps.cn/?qrcode=kdocs&logo=kdocs&accessid=AK20210823OPGONG&from=v1-web-kdocs-login&cb=https%3A%2F%2Faccount.wps.cn%2Fapi%2Fv3%2Fsession%2Fcorrelate%2Fredirect%3Ft%3D1661241340991%26appid%3D375024576%26cb%3Dhttps%253A%252F%252Fwww.kdocs.cn%252FsingleSign4CST%253Fcb%253Dhttps://www.kdocs.cn/l/coO0iEfp4s1c";
+
   //get Application(): any {
   //  return (async () => {return await this.jssdk.Application;})()
   //}
 
 
 
-  start = async () => {
-    await this.jssdk.ready();
 
-
-
-
-    //const range = await this.jssdk.Application.Range('A1')
-
-    // 设置公式：A1   = A2 + A3
-    //range.Formula = '=A2+A3'
-
-  }
 }
-window.onload = () => {
-  new app();
-}
+
