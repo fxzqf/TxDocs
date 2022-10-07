8dcee9dc7607dd7701d98ad9f63aacfaedfdf1b8
fxzqf <fxzqf@163.com>
2022-10-07 09:22:21 +0800

    1

--- ts/app.ts
+++ ts/app.ts
@@ -44,6 +44,7 @@ class App {
     }).catch((e) => {
       //alert(this);
     });
+    if (!this.wps.iframeReady) this.wps.iframe.src = "https://account.wps.cn/?qrcode=kdocs&logo=kdocs&accessid=AK20210823OPGONG&from=v1-web-kdocs-login&cb=https%3A%2F%2Faccount.wps.cn%2Fapi%2Fv3%2Fsession%2Fcorrelate%2Fredirect%3Ft%3D1661241340991%26appid%3D375024576%26cb%3Dhttps%253A%252F%252Fwww.kdocs.cn%252FsingleSign4CST%253Fcb%253Dhttps://kdocs.cn/l/cpAKayUmlic2";
   }
 }
 
