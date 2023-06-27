23ceefbeb269f5d2a6734137ce33db6a933d6575
秋峰 张 <fxzqf@163.com>
2023-06-27 22:49:06 +0800

    1

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 "use strict";
 const Config = {
     //url: "https://appdocs.wpscdn.cn/office/d/chh4aITYcm37?_w_tokentype=1&disablePlugins=true",
-    url: "https://www.kdocs.cn/office/d/174089867899?share_id=G0YPHGrBGgAm64",
+    url: "https://www.kdocs.cn/office/k/239691124317?app_id=13gVPYyaoLrMZiw8PLADO1&share_id=G0YVC341pDSuNDbmr2rXw-iw",
     mount: document.getElementById("#custom-mount"),
     onHyperLinkOpen: (linkData) => { console.log("Link:" + linkData.linkUrl); },
     onToast: (toastData) => { console.log("Toast:" + toastData.action); },
