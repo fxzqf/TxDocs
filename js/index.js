e379ba7d4533515aba166b4f325b6e06b4d8864d
秋峰 张 <fxzqf@163.com>
2023-08-23 21:16:33 +0800

    1

--- js/index.js
+++ js/index.js
@@ -22,11 +22,10 @@ window.onload = () => {
              }
          }*/
     sha1 = new jsSHA("SHA-1", "TEXT", { encoding: "UTF8" });
-    jsticket= "sM4AOVdWfPE4DxkXGEs8VIg3S6LumlLSveNvpaIZuJnKdc1o2xmlvDn_PspGaQRH2UKGu3odtr94QIgkYodDgw"
     wx.config({
         debug: false,
         appId: 'wx8301d806150dba74',
-        timestamp:1692825277,
+        timestamp: Math.trunc(Date.now() / 1000),
         nonceStr: 'fxzqf',
         signature: sha1.update("jsapi_ticket=" + jsticket + "&" + "noncestr=fxzqf&" + "timestamp=" + timestamp + "&" + "url=" + window.location.href).getHash("HEX"),
         jsApiList: [
